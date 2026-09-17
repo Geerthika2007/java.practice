f376e3485ae379ef023bb8fe4ab3f3362d0da4b0
Geerthika2006 <rgeerthika2006@gmail.com>
2026-09-16 17:40:08 -0700

    Added Add Two Nummbers program

---
+++ AddTwonumbers.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+ class AddTwonumbers{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first numbers: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second numbers: ");
+        int b = sc.nextInt();
+    int sum =a+b;
+    System.out.println("sum of two numbers is: "+sum);
+    }
+ }
